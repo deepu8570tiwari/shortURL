e17976ae137df9026a510bd91cfb0db1e5ad19e6
Deepu <itsdeeputiwari@gmail.com>
2025-12-08 21:12:56 +0530

    make some basic changes

--- shortUrl/src/configs/database.js
+++ shortUrl/src/configs/database.js
@@ -1,8 +1,9 @@
 const mongoose = require("mongoose");
-
+const dotenv=require("dotenv");
+dotenv.config();
 async function connectDB() {
   try {
-    await mongoose.connect("mongodb://mongo:27017/shortUrl");
+    await mongoose.connect(process.env.MONGO_DB);
     console.log("Connected to MongoDB");
   } catch (error) {
     console.error("MongoDB connection error:", error);
--- shortUrl/src/index.js
+++ shortUrl/src/index.js
@@ -6,6 +6,10 @@ const urlRouter=require("./routes/urlRouter");
 const app=express();
 app.use(express.json());
 app.use("/api/v1/",urlRouter);
+app.get("/", (req, res) => {
+    res.send("Hello Your code is working");
+});
+
 app.listen(process.env.NODE_PORT, ()=>{
     console.log("Server is running on Port",`${process.env.NODE_PORT}`)
     connectDB();
