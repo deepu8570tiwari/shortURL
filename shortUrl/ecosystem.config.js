e8a2ae3dc201a86fce590f4a5939a5a09a18ed3e
Deepu <itsdeeputiwari@gmail.com>
2025-12-09 10:30:52 +0530

    ci/cd

--- shortUrl/ecosystem.config.js
+++ shortUrl/ecosystem.config.js
@@ -1,10 +1,12 @@
 import dotenv from "dotenv";
 dotenv.config();
-
 module.exports = {
   apps: [
     {
       name: "short-url",
+      instances: 1,
+      autorestart: true,
+      watch: false,
       script: "./src/index.js",
       env: {
         MONGO_URI: process.env.MONGO_URI,
